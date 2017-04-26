3795a57f38455251c2d11ef0364677a49e69372a
ultrapla <ultrapla.29@gmail.com>
2017-04-26 10:43:57 +0700

    Step5 code varible

--- app/src/main/java/cph/nakhundee/wannapa/mycphos/MainActivity.java
+++ app/src/main/java/cph/nakhundee/wannapa/mycphos/MainActivity.java
@@ -2,12 +2,33 @@ package cph.nakhundee.wannapa.mycphos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit คือการประการศตัวแปร
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initial View การผู้ตัวแปร
+        initialView();
+
+    }   // นี Main Method ่คือ method หลัก
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);  //cart เปลี่ยนตัวแปร
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.textNewRegis);
+        button = (Button) findViewById(R.id.btnLogin);
     }
-}
+
+
+}   // Main Class นี่คือคลาสหลัก
